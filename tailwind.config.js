efb4cff5886525f9ab050e079c6e5033d7d04c22
isb <jaredlamont90@gmail.com>
2025-09-25 00:21:50 -0700

    Added: tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+content: [
+  './app/**/*.{js,ts,jsx,tsx,mdx}',
+  './pages/**/*.{js,ts,jsx,tsx,mdx}',
+  './components/**/*.{js,ts,jsx,tsx,mdx}',
+  // if you have a src/ folder or a package workspace, include those too:
+  './src/**/*.{js,ts,jsx,tsx,mdx}',
+  '../../packages/ui/**/*.{js,ts,jsx,tsx,mdx}', // monorepo example
+]
